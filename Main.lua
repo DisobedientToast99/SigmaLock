fbb771d9fb2ed4fa2bb7c556b8f9b45e311853c2
DisobedientToast99 <overeclipsed.mc@gmail.com>
2024-12-10 04:10:35 -0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,5 @@
-loadstring(game:GetObjects('rbxassetid://18626311725')[1].Source)()
+if game:GetObjects('rbxassetid://18626311725') then
+  loadstring(game:GetObjects('rbxassetid://18626311725')[1].Source)()
+else
+  loadstring("https://raw.githubusercontent.com/DisobedientToast99/sigmalock/refs/heads/main/Source.lua")()
+end
