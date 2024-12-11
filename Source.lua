e7f2a0273db066c748bbcf8899da79baa76dc1dc
DisobedientToast99 <overeclipsed.mc@gmail.com>
2024-12-10 18:02:57 -0600

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -441,7 +441,7 @@ local function RunGui()
 	gui.Info.ESP.Text = "ESP: "..(data.ESP and "On" or "Off")
 	gui.Info.FreeForAll.Text = "FFA: "..(data.FreeForAll and "On" or "Off")
 	gui.Info.LockingType.Text = "Type: "..data.LockingType
-	gui.Info.TriggerBot.Text = "Trigger Bot: "..data.TriggerBot
+	gui.Info.TriggerBot.Text = "Trigger Bot: "..(data.TriggerBot and "On" or "Off")
 	
 	for _, frame in pairs(gui:GetChildren()) do
 		if frame:IsA("Frame") and frame.BackgroundTransparency ~= data.GuiTransparency then
